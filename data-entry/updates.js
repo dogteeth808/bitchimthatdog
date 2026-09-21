33ad70518054e90b6f0afde7eb14ff7f2ab44959
dogteeth808 <155699671+dogteeth808@users.noreply.github.com>
2026-09-21 07:06:39 -0700

    new update

--- data-entry/updates.js
+++ data-entry/updates.js
@@ -11,6 +11,7 @@ const updateEntries = [
 {date:"2026 08 19", text:`Followed  <a href="https://www.youtube.com/watch?v=p1QU3kLFPdg">this lovely tutorial</a> to get this moved to github pages on my custom domain!!! exciting developments` },  
 {date:"2026 08 24", text:`HUGE one today!!! A rudimentary version of the music section is online. I wrote some JavaScript to turn a list of data into a list of clickable song entries! And I can change the type of list it generates in no time at all! Go me!!!` },
 {date:"2026 09 15", text:`Category buttons on music page work properly! The "latest release" section on this page works too! Basically all of the music page infrastructure shit is done. All design from this point on! Should be able to reuse some of that for the blog too, which is also exciting. Yippee!` },
+{date:"2026 09 21", text:`Buttons are now not just me a billion times. A lot more units are based on VH. New "keyboard" images beside Music and Blog on the index, and some experimental texturing on the music page!` },
 ]
 
 function updatesCompileIndex() {
